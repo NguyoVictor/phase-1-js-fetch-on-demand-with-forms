f486d89b864014820c0f14e3a38ec29ec14e504a
Victor Nguyo <victor.nguyo@student.moringaschool.com>
2024-04-09 19:10:44 +0300

    completelab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+  const formInput = document.querySelector("form");
+  formInput.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("form #searchByID");
+    console.log(input.value);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+        title.innerText = data.title;
+        console.log(data.title);
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
